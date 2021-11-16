7fb605f32fc63c5a48d93489fa6aff329089e467
GilmaD <84815208+GilmaD@users.noreply.github.com>
2021-11-16 15:44:27 -0600

    Add files via upload

---
+++ Reposteria-main/Win.Reposteria/FormReporteProducto.cs
@@ -0,0 +1,31 @@
+﻿using BL.Reposteria;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Rentas
+{
+    public partial class FormReporteProducto : Form
+    {
+        public FormReporteProducto()
+        {
+            InitializeComponent();
+
+            var _productoBL = new ProductosBL();
+            var bindingSource = new BindingSource();
+            bindingSource.DataSource = _productoBL.ObtenerProductos();
+
+            var reporte = new ReporteProductos();
+            reporte.SetDataSource(bindingSource);
+
+            crystalReportViewer1.ReportSource = reporte;
+            crystalReportViewer1.RefreshReport();
+        }
+    }
+}
