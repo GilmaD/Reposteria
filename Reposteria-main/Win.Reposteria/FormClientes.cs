9841cb348c0b4bffffe96d9e57c51e651156ee96
GilmaD <84815208+GilmaD@users.noreply.github.com>
2021-11-24 00:04:33 -0600

    Add files via upload

---
+++ Reposteria-main/Win.Reposteria/FormClientes.cs
@@ -0,0 +1,127 @@
+﻿using BL.Reposteria;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Reposteria
+{
+    public partial class FormClientes : Form
+    {
+        ClientesBL _clientes;
+        SexosBL _sexos;
+
+        public FormClientes()
+        {
+            InitializeComponent();
+
+            _clientes = new ClientesBL();
+            listaClientesBindingSource.DataSource = _clientes.ObtenerClientes();
+
+            _sexos = new SexosBL();
+            listaSexosBindingSource.DataSource = _sexos.ObtenerSexos();
+        }
+
+        private void listaClientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaClientesBindingSource.EndEdit();
+            var cliente = (Cliente)listaClientesBindingSource.Current;
+
+            var resultado = _clientes.GuardarCliente(cliente);
+
+            if (resultado.Exitoso == true)
+            {
+                listaClientesBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+                MessageBox.Show("Cliente guardado");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _clientes.AgregarCliente();
+            listaClientesBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            
+               if (idTextBox.Text != "")
+               {
+                   var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+
+                   if (resultado == DialogResult.Yes)
+                   {
+                       var id = Convert.ToInt32(idTextBox.Text);
+                       Eliminar(id);
+                   }
+               }
+
+            
+         }
+
+        private void Eliminar(int id)
+        {
+            
+            var resultado = _clientes.EliminarCliente(id);
+
+            if (resultado == true)
+            {
+                listaClientesBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar el cliente");
+            }
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+            _clientes.CancelarCambios();
+            DeshabilitarHabilitarBotones(true);
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var buscar = textBox1.Text;
+
+            if (string.IsNullOrEmpty(buscar) == true)
+            {
+                listaClientesBindingSource.DataSource =
+                    _clientes.ObtenerClientes();
+            }
+            else
+            {
+                listaClientesBindingSource.DataSource =
+                    _clientes.ObtenerClientes(buscar);
+            }
+
+            listaClientesBindingSource.ResetBindings(false);
+        }
+    }
+}
---
+++ Reposteria-main/Win.Reposteria/FormProveedores.cs
@@ -0,0 +1,106 @@
+﻿using BL.Reposteria;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Reposteria
+{
+    public partial class FormProveedores : Form
+    {
+        ProveedoresBL _proveedores;
+        LocalidadesBL _localidades;
+
+        public FormProveedores()
+        {
+            InitializeComponent();
+
+            _proveedores = new ProveedoresBL();
+            listaProveedoresBindingSource.DataSource = _proveedores.ObtenerProveedores();
+
+            _localidades = new LocalidadesBL();
+            listaLocalidadesBindingSource.DataSource = _localidades.ObtenerLocalidades();
+        }
+
+        private void listaProveedoresBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            listaProveedoresBindingSource.EndEdit();
+            var proveedor = (Proveedor)listaProveedoresBindingSource.Current;
+
+            var resultado = _proveedores.GuardarProveedor(proveedor);
+
+            if (resultado.Exitoso == true)
+            {
+                listaProveedoresBindingSource.ResetBindings(false);
+                DeshabilitarHabilitarBotones(true);
+                MessageBox.Show("Proveedor guardado");
+            }
+            else
+            {
+                MessageBox.Show(resultado.Mensaje);
+            }
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            _proveedores.AgregarProveedor();
+            listaProveedoresBindingSource.MoveLast();
+
+            DeshabilitarHabilitarBotones(false);
+        }
+
+        private void DeshabilitarHabilitarBotones(bool valor)
+        {
+            bindingNavigatorMoveFirstItem.Enabled = valor;
+            bindingNavigatorMoveLastItem.Enabled = valor;
+            bindingNavigatorMovePreviousItem.Enabled = valor;
+            bindingNavigatorMoveNextItem.Enabled = valor;
+            bindingNavigatorPositionItem.Enabled = valor;
+
+            bindingNavigatorAddNewItem.Enabled = valor;
+            bindingNavigatorDeleteItem.Enabled = valor;
+            toolStripButtonCancelar.Visible = !valor;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+           if (idTextBox.Text != "")
+           {
+               var resultado = MessageBox.Show("Desea eliminar este registro?", "Eliminar", MessageBoxButtons.YesNo);
+               if (resultado == DialogResult.Yes)
+               {
+                   var id = Convert.ToInt32(idTextBox.Text);
+                   Eliminar(id);
+               }
+                           
+            }
+        }
+
+        private void Eliminar(int id)
+        {
+            
+            var resultado = _proveedores.EliminarProveedor(id);
+
+            if (resultado == true)
+            {
+                listaProveedoresBindingSource.ResetBindings(false);
+            }
+            else
+            {
+                MessageBox.Show("Ocurrio un error al eliminar el proveedor");
+            }
+        }
+
+        private void toolStripButtonCancelar_Click(object sender, EventArgs e)
+        {
+            _proveedores.CancelarCambios();
+            DeshabilitarHabilitarBotones(true);
+           
+        }
+    }
+}
