9be2e80229d2c1bf61e62cf715baecdd9b2a430e
GilmaD <84815208+GilmaD@users.noreply.github.com>
2021-11-24 00:01:21 -0600

    Add files via upload

---
+++ Reposteria-main/FormLogin.cs
@@ -0,0 +1,79 @@
+﻿using BL.Reposteria;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Win.Reposteria
+{
+    public partial class FormLogin : Form
+    {
+        SeguridadBL _seguridad;
+
+        public FormLogin()
+        {
+            InitializeComponent();
+
+            _seguridad = new SeguridadBL();
+        }
+
+        private void FormLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string usuario;
+            string contrasena;
+
+            usuario = textBox1.Text;
+            contrasena = textBox2.Text;
+
+            button1.Enabled = false;
+            button1.Text = "Verificando...";
+            Application.DoEvents();
+
+            var usuarioDB = _seguridad.Autorizar(usuario, contrasena);
+
+            if (usuarioDB != null)
+            {
+                Program.UsuarioLogueado = usuarioDB;
+
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Usuario o contraseña incorrecta");
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)
+                && !string.IsNullOrEmpty(textBox1.Text))
+            {
+                textBox2.Focus();
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Enter)
+                && !string.IsNullOrEmpty(textBox2.Text))
+            {
+                button1.PerformClick();
+            }
+        }
+    }
+}
