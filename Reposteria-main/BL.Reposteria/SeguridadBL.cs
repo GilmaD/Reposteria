e0df86625dc88bfa5dc1c1300756f2b1535c08fb
GilmaD <84815208+GilmaD@users.noreply.github.com>
2021-11-24 00:08:31 -0600

    Add files via upload

---
+++ Reposteria-main/BL.Reposteria/SeguridadBL.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Reposteria
+{
+    public class SeguridadBL
+    {
+        Contexto _contexto;
+
+        public SeguridadBL()
+        {
+            _contexto = new Contexto();
+        }
+
+        public Usuario Autorizar(string nombreUsuario, string contrasena)
+        {
+            var usuarios = _contexto.Usuarios.ToList();
+
+            foreach (var usuarioDB in usuarios)
+            {
+                if (nombreUsuario == usuarioDB.Nombre && contrasena == usuarioDB.Contrasena)
+                {
+                    return usuarioDB;
+                }
+            }
+
+            return null;
+        }
+    }
+
+
+}
+    
+
+
+    public class Usuario
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; }
+            public string Contrasena { get; set; }
+            
+        }
+
+    
+
---
+++ Reposteria-main/BL.Reposteria/SexosBL.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Reposteria
+{
+    public class SexosBL
+    {
+        Contexto _contexto;
+        public BindingList<Sexo> ListaSexos { get; set; }
+
+        public SexosBL()
+        {
+            _contexto = new Contexto();
+            ListaSexos = new BindingList<Sexo>();
+        }
+
+        public BindingList<Sexo> ObtenerSexos()
+        {
+            _contexto.Sexos.Load();
+            ListaSexos = _contexto.Sexos.Local.ToBindingList();
+
+            return ListaSexos;
+        }
+    }
+
+          
+
+    public class Sexo
+    {
+        public int Id { get; set; }
+        public string Sexos { get; set; }
+    }
+}
